c90fbfba813477cf407b1c12778a096a3403cb5e
Tchinonso <lightersmercury@gmail.com>
2023-01-20 18:06:41 -0800

    new commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+function App() {
+    return ( 
+        <div className = "App" >
+         <h1> hello </h1> 
+         <p> how are you </p>
+        </div>
+    );
+}
+
+export default App;
